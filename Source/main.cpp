3c0b39367001feb06c3f3fa603a714fb831f9a00
ZipiRo <ionutperdun777@gmail.com>
2026-05-08 17:28:24 +0300

    Update last updated date in VersionOption to 05/08/2026 and increment version to 1.0.8

--- Source/main.cpp
+++ Source/main.cpp
@@ -137,7 +137,7 @@ void VersionOption()
                 "Created to simplify SFML project creation, and makeing it easier with including code\n"
                 "and linking libraryes for any other things you want to add to your project.\n"
                 "Surely works with SFML-2.6.2, SFML-3.0.0, more in  the future.\n"
-                "Created on 05/05/2026 and Last Updated 05/07/2026.\n"
+                "Created on 05/05/2026 and Last Updated 05/08/2026.\n"
                 "PicSFML Version 1.0.8\n";
 }
 
