45cfb7d034ac8ac49d88dcc1af15683aea6fbe80
ZipiRo <ionutperdun777@gmail.com>
2026-05-28 01:45:14 +0300

    Update last updated date in version option output

--- Source/version_option.h
+++ Source/version_option.h
@@ -5,6 +5,6 @@ void VersionOption()
         "Created to simplify SFML project creation, and makeing it easier with including code\n"
         "and linking libraryes for any other things you want to add to your project.\n"
         "Surely works with SFML-2.6.2, SFML-3.0.0, more in  the future.\n"
-        "Created on 05/05/2026 and Last Updated 05/25/2026.\n"
+        "Created on 05/05/2026 and Last Updated 05/28/2026.\n"
         "PicSFML Version " << PICSFML_VERSION.AsString('.') << "\n";
 }
